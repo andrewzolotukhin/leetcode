354c5c22e72ccf62485783dd9655bebe89810ea3
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-10-29 17:15:41 +0200

    P00009 accepted (version without coverting to string)

--- P00009PalindromeNumber/P00009-PalindromeNumber.cs
+++ P00009PalindromeNumber/P00009-PalindromeNumber.cs
@@ -8,6 +8,7 @@ public class P00009_PalindromeNumber
 	{
 		if (x < 0) return false;
 		if (x == 0) return true;
+		// version with conversion to string
 		var s = x.ToString();
 
 		for (var i = 0; i < s.Length / 2; i++)
@@ -16,5 +17,34 @@ public class P00009_PalindromeNumber
 		}
 
 		return true;
+
+
+		// version without converting to string
+
+		// var digits = new byte[10];
+		// int divider = 1000000000;
+		// int k = x;
+		// var i = 0;
+		// while (divider > 0)
+		// {
+		// 	digits[i] = (byte)(k / divider);
+		// 	if (digits[i] > 0)
+		// 	{
+		// 		k -= digits[i] * divider;
+		// 	}
+		// 	i++;
+		// 	divider /= 10;
+		// }
+
+		// var nonZeroDigits = digits.SkipWhile(t => t == 0).ToArray();
+
+		// if (nonZeroDigits.Length == 0) return true;
+
+		// for (i = 0; i < nonZeroDigits.Length / 2; i++)
+		// {
+		// 	if (nonZeroDigits[i] != nonZeroDigits[nonZeroDigits.Length - i - 1]) return false;
+		// }
+
+		// return true;
 	}
 }
