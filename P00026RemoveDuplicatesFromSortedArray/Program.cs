41d0fe41772c75aebd0b503b649abbb2e11be560
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-11-18 05:13:43 +0200

    P00026: accepted (optimal)

--- P00026RemoveDuplicatesFromSortedArray/Program.cs
+++ P00026RemoveDuplicatesFromSortedArray/Program.cs
@@ -7,36 +7,49 @@ a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 result = Solution.RemoveDuplicates(a);
 Console.WriteLine(result);
 Console.WriteLine(string.Join(",", a));
+Console.WriteLine("======================");
+a = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1];
+result = Solution.RemoveDuplicates(a);
+Console.WriteLine(result);
+Console.WriteLine(string.Join(",", a));
+Console.WriteLine("======================");
+a = [0, 0, 0, 0, 0];
+result = Solution.RemoveDuplicates(a);
+Console.WriteLine(result);
+Console.WriteLine(string.Join(",", a));
 
 public class Solution
 {
 	public static int RemoveDuplicates(int[] nums)
 	{
 		var i = 0;
-		var ranges = new List<(int, int)>();
 
 		if (nums.Length == 0) return 0;
 
 		var rangeStart = 0;
+		var uniq = 0;
 
 		while (++i < nums.Length)
 		{
+			if (nums[i - 1] == nums[nums.Length - 1])
+			{
+				nums[uniq] = nums[rangeStart];
+				uniq++;
+				return uniq;
+			}
+
 			if (nums[rangeStart] != nums[i])
 			{
-				ranges.Add((rangeStart, i - 1));
+				nums[uniq] = nums[rangeStart];
+				uniq++;
 				rangeStart = i;
 			}
 
 		}
 
-		ranges.Add((rangeStart, nums.Length - 1));
+		nums[uniq] = nums[nums.Length - 1];
+		uniq++;
 
-		i = 0;
-		foreach (var (k, _) in ranges)
-		{
-			nums[i++] = nums[k];
-		}
-
-		return ranges.Count;
+		return uniq;
 	}
 }
