c2021be09da97c917fe428fca0e97761c4c51cda
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2024-11-19 02:41:19 +0200

    P00029: accepted - optimal, bit operations

--- P00029DivideTwoIntegers/Program.cs
+++ P00029DivideTwoIntegers/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection.Metadata.Ecma335;
+
 var test = (int d, int i) =>
 {
 	var r = Solution.Divide(d, i);
@@ -30,7 +32,14 @@ test(int.MaxValue, -1);
 test(int.MinValue, -1);
 test(int.MinValue, int.MaxValue);
 test(int.MaxValue, int.MinValue);
+test(234455344, 3433);
+test(234455344, -3433);
+test(-234455344, -3433);
+test(-234455344, 3433);
 test(0, 0);
+test(-2147483648, 2);
+test(-2147483648, 4);
+test(-1658356636, -1639504005);
 
 public class Solution
 {
@@ -38,52 +47,51 @@ public class Solution
 	{
 		if (dividend == 0) return 0;
 		if (dividend == divisor) return 1;
+		if (dividend == int.MinValue && divisor == -1) return int.MaxValue;
 		if (divisor == 1) return dividend;
-		if (divisor == -1) return dividend switch { int.MaxValue => -int.MaxValue, int.MinValue => int.MaxValue, _ => -dividend };
+		if (divisor == -1) return -dividend;
+		if (divisor == int.MinValue) return 0;
 
 		var isDividentNegative = dividend < 0;
 		var isDivisorNegative = divisor < 0;
 		var isResultNegative = isDividentNegative ^ isDivisorNegative;
 		if (divisor == 0) return isResultNegative ? int.MinValue : int.MaxValue;
 
-		var result = 0;
 
-		if ((dividend, divisor) is ( < 0, < 0))
+		Func<int, uint> touint = (int input) => (uint)((input < 0 ? (~input) : input) & 0x7FFFFFFF) + (uint)((input < 0) ? 1 : 0);
+
+		var d = touint(dividend);
+		var r = touint(divisor);
+
+		uint result = 0;
+		uint m, k;
+		int i = 0;
+
+		while (d >= r)
 		{
-			while (dividend <= divisor)
+			m = r;
+			k = 1;
+			i = 0;
+			while (m <= d && m <= 0x70000000)
 			{
+				m <<= 1;
+				k <<= 1;
+				i++;
+			}
+
+			if (k >> 1 > 0)
+			{
+				result += k >> 1;
+				d -= m >> 1;
+			}
+			else
+			{
+				d -= r;
 				result++;
-				dividend -= divisor;
 			}
-		}
-		else if ((dividend, divisor) is ( < 0, > 0))
-		{
-			while (dividend <= 0)
-			{
-				result--;
-				dividend += divisor;
-			}
-			result++;
-		}
-		else if ((dividend, divisor) is ( > 0, < 0))
-		{
-			while (dividend >= 0)
-			{
-				result--;
-				dividend += divisor;
-			}
-			result++;
-		}
-		else if ((dividend, divisor) is ( > 0, > 0))
-		{
-			while (dividend >= divisor)
-			{
-				result++;
-				dividend -= divisor;
-			}
+
 		}
 
-
-		return result;
+		return (int)result * (isResultNegative ? -1 : 1);
 	}
 }
