b7839f55f691c9326318434d836782f1dee0b29f
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-01-04 03:12:46 +0200

    P983: accepted

--- P00983MinimumCostForTickets/solution.cpp
+++ P00983MinimumCostForTickets/solution.cpp
@@ -3,7 +3,38 @@ using namespace std;
 
 class Solution {
 public:
-  int mincostTickets(vector<int> &days, vector<int> &costs) {}
+  int solve(int day, vector<bool> &daysWithTravel, vector<int> &dp,
+            vector<int> &costs) {
+    if (day > dp.size() - 1) {
+      return 0;
+    }
+    if (dp[day] != -1) {
+      return dp[day];
+    }
+
+    int result = 0;
+
+    if (daysWithTravel[day]) {
+      result = min(min(costs[0] + solve(day + 1, daysWithTravel, dp, costs),
+                       costs[1] + solve(day + 7, daysWithTravel, dp, costs)),
+                   costs[2] + solve(day + 30, daysWithTravel, dp, costs));
+    } else {
+      result = solve(day + 1, daysWithTravel, dp, costs);
+    }
+
+    dp[day] = result;
+    return result;
+  }
+
+  int mincostTickets(vector<int> &days, vector<int> &costs) {
+    vector<bool> daysWithTravel(365, false);
+    vector<int> dp(366, -1);
+    for (auto day : days) {
+      daysWithTravel[day - 1] = true;
+    }
+
+    return solve(0, daysWithTravel, dp, costs);
+  }
 };
 
 int main() {
