298814d4551446aef778b7b9a1ee10595be40bda
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-02-17 03:34:23 +0200

    contest 437: q3 (w/ editorial)

--- contests/437/q3/solution.cpp
+++ contests/437/q3/solution.cpp
@@ -7,16 +7,61 @@ public:
     if (k == 0)
       return true;
 
-    vector<vector<int>> indexes(26, vector<int>());
+    vector<pair<int, int>> minMax(26, {-1, -1});
 
     for (int i = 0; i < s.size(); i++) {
       int index = s[i] - 'a';
-      indexes[index].push_back(i);
+      minMax[index].first =
+          minMax[index].first == -1 ? i : min(minMax[index].first, i);
+      minMax[index].second = max(minMax[index].second, i);
     }
 
+    vector<pair<int, int>> candidates;
+
     int n = s.size();
-    // next power of 2 greater or equal to n+1
-    int treeSize = 1 << (int)ceil(log2(n + 1));
+
+    for (int i = 0; i < n; i++) {
+      int index = s[i] - 'a';
+      if (minMax[index].first != i) {
+        continue;
+      }
+
+      int start = i;
+      int end = minMax[index].second;
+
+      for (int j = i + 1; j <= end; j++) {
+        int index2 = s[j] - 'a';
+        if (minMax[index2].first < start) {
+          start = minMax[index2].first;
+          end = max(end, minMax[index2].second);
+          j = start;
+        }
+        if (minMax[index2].second > end) {
+          end = minMax[index2].second;
+        }
+      }
+
+      if (!(start == 0 && end == n - 1)) {
+        candidates.push_back({start, end});
+      }
+    }
+
+    sort(candidates.begin(), candidates.end(),
+         [&](pair<int, int> a, pair<int, int> b) {
+           return a.second < b.second;
+         });
+
+    int found = 0;
+    int last = -1;
+
+    for (auto candidate : candidates) {
+      if (candidate.first > last) {
+        found++;
+        last = candidate.second;
+      }
+    }
+
+    return found >= k;
   }
 };
 
