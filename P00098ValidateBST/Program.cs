ddc23895fb895982ef6bb68593f09bf22210e2a0
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-04-14 02:22:14 +0300

    P98: accepted

---
+++ P00098ValidateBST/Program.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private bool isValid(TreeNode node, long min, long max) {
+        if(node == null) return true;
+        if(node.val <= min || node.val >= max) return false;
+        return isValid(node.left, min, node.val) && isValid(node.right, node.val, max);
+    }
+    public bool IsValidBST(TreeNode root) {
+        long m = 100000000000;
+        return isValid(root, -m, m);
+    }
+}
