3e12abc5266de612174963a34c3654b04e01968e
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-04-20 05:04:41 +0300

    P2637: accepted

---
+++ P02637PromiseTimeLimit/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+  return async function (...args) {
+    let to = new Promise((r, j) => setTimeout(() => j("Time Limit Exceeded"), t));
+    return await Promise.race([fn(...args), to]);
+  };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
