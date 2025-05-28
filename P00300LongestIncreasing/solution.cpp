1ee00c582b9cb9bbb220fdea88cf729a99d678a5
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-05-28 03:21:20 +0300

    P300: accepted

---
+++ P00300LongestIncreasing/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int lengthOfLIS(vector<int> &nums) {
+    vector<int> dp(nums.size(), 1);
+    int ans = 1;
+
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = 0; j < i; j++) {
+        if (nums[i] > nums[j]) {
+          dp[i] = max(dp[j] + 1, dp[i]);
+          ans = max(ans, dp[i]);
+        }
+      }
+    }
+
+    return ans;
+  }
+};
