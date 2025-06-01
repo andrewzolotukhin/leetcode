61aa81c33db607e968ecc25d43f880e6b7124b62
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-06-02 02:18:15 +0300

    P224: accepted

--- P00224BasicCalculator/Program.cs
+++ P00224BasicCalculator/Program.cs
@@ -1,15 +1,57 @@
-﻿using System;
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var s = new Solution();
 
-Console.
-
-var b = Test.Sum(10, 20);
-
-public static class Test
+var test = (string expression, int expected) =>
 {
-    public static void Sum(int a, int b)
+    var result = s.Calculate(expression);
+    if (result != expected)
     {
-        return a + b;
+        Console.WriteLine("Failed: " + expression + " Expected: " + expected + " Got: " + result);
+    }
+};
+
+test("10+20*2", 50); // 50
+test("10+20*2+3", 53); // 53
+test("10+20/2+3", 23); // 23
+test("(10+20)/2+3", 18); // 18
+
+public class Solution
+{
+    public int Calculate(string s)
+    {
+        int len = s.Length, sign = 1, result = 0;
+        Stack<int> stack = new();
+        for (int i = 0; i < len; i++)
+        {
+            if (char.IsDigit(s[i]))
+            {
+                int sum = s[i] - '0';
+                while (i + 1 < len && char.IsDigit(s[i + 1]))
+                {
+                    sum = sum * 10 + s[i + 1] - '0';
+                    i++;
+                }
+                result += sum * sign;
+            }
+            else if (s[i] == '+')
+                sign = 1;
+            else if (s[i] == '-')
+                sign = -1;
+            else if (s[i] == '(')
+            {
+                stack.Push(result);
+                stack.Push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if (s[i] == ')')
+            {
+                result = result * stack.Pop() + stack.Pop();
+            }
+
+        }
+        return result;
+
+
     }
 }
+
