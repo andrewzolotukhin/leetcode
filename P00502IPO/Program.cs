237bc5afecf0c65aa135cc1ef6f678b4d56c9dd4
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-06-02 16:49:08 +0300

    P502: accepted

---
+++ P00502IPO/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+	public int FindMaximizedCapital(int k, int w, int[] profits, int[] capital)
+	{
+		Array.Sort(capital, profits);
+		var queue = new PriorityQueue<int, int>();
+		int index = 0;
+		while (k > 0)
+		{
+			while (index < profits.Length && capital[index] <= w)
+			{
+				queue.Enqueue(profits[index], -profits[index]);
+				index++;
+			}
+			if (queue.Count == 0) break;
+			w += queue.Dequeue();
+			k--;
+		}
+		return w;
+	}
+}
