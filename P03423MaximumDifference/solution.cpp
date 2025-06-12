6b04032ebdc2033c9bc30c7f613a6288fe0effc1
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-06-12 03:09:03 +0300

    P3423: accepted

---
+++ P03423MaximumDifference/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+  int maxAdjacentDistance(vector<int> &nums) {
+    int ans = INT_MIN;
+    int n = nums.size();
+    for (int i = 0; i <= nums.size(); i++) {
+      ans = max(ans, abs(nums[i % n] - nums[(i + 1) % n]));
+    }
+    return ans;
+  }
+};
