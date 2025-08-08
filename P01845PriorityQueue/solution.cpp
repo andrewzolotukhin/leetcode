e217f6310afb7f641ea98909d1d3b7d2ce15ccee
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-08-08 23:42:35 +0300

    P1845: accepted

---
+++ P01845PriorityQueue/solution.cpp
@@ -0,0 +1,27 @@
+class SeatManager {
+private:
+  priority_queue<int, vector<int>, greater<>> *pq;
+
+public:
+  SeatManager(int n) {
+    pq = new priority_queue<int, vector<int>, greater<>>();
+    for (int i = 1; i <= n; i++) {
+      pq->push(i);
+    }
+  }
+
+  int reserve() {
+    int r = pq->top();
+    pq->pop();
+    return r;
+  }
+
+  void unreserve(int seatNumber) { pq->push(seatNumber); }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
