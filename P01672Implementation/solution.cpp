b23117a0d222c0d19ae367b476d226099d635b77
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-09-12 04:43:43 +0300

    P1672: accepted

---
+++ P01672Implementation/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int maximumWealth(vector<vector<int>> &accounts) {
+    int n = accounts.size(), m = accounts[0].size();
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      int s = 0;
+      for (int j = 0; j < m; j++) {
+        s += accounts[i][j];
+      }
+
+      if (s > ans)
+        ans = s;
+    }
+
+    return ans;
+  }
+};
