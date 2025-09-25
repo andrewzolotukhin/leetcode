5e6367f1486faf821ac6fb0233309cfc91b21b98
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-09-25 04:07:49 +0300

    P1393: accepted

---
+++ P01393Sql/query.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+
+select 
+  stock_name,
+  sum(CASE WHEN operation = 'Buy' THEN -price ELSE price END) as capital_gain_loss
+from
+  Stocks
+group by stock_name;
