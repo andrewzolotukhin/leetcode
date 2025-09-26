e65c623524d7e085ff4022e01c2395e7ba717480
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-09-26 16:31:13 +0300

    P950: accepted

---
+++ P00950Simulation/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  vector<int> deckRevealedIncreasing(vector<int> &deck) {
+    sort(deck.begin(), deck.end());
+    queue<int> q;
+    for (int i = 0; i < deck.size(); i++) {
+      q.push(i);
+    }
+
+    vector<int> ans(deck.size());
+
+    for (int i = 0; i < deck.size(); i++) {
+      ans[q.front()] = deck[i];
+      q.pop();
+
+      if (!q.empty()) {
+        q.push(q.front());
+        q.pop();
+      }
+    }
+    return ans;
+  }
+};
