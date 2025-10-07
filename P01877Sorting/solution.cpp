04fadac3392be01985a073879bb73ce48b45932a
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-10-07 04:26:03 +0300

    P1877: accepted

---
+++ P01877Sorting/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int minPairSum(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+
+    int n = nums.size(), ans = INT_MIN;
+    for (int i = 0; i < n / 2; i++) {
+      ans = max(ans, nums[i] + nums[n - 1 - i]);
+    }
+
+    return ans;
+  }
+};
