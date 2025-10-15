71f74a4e26e5c3fea5434912f579330a2e22f131
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2025-10-15 06:18:46 +0300

    P1310: accepted

---
+++ P01310PrefixSums/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  vector<int> xorQueries(vector<int> &arr, vector<vector<int>> &queries) {
+    int n = arr.size();
+    vector<int> prefix(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+      prefix[i] = prefix[i - 1] ^ arr[i - 1];
+    }
+
+    vector<int> ans;
+
+    for (auto q : queries) {
+      ans.push_back(prefix[q[0]] ^ prefix[q[1] + 1]);
+    }
+
+    return ans;
+  }
+};
