51dc30f7c7c62fbc3880fa7ccf4fb52cdd7c8f83
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2026-02-25 11:17:59 +0200

    P1356: accepted

---
+++ P01356Sorting/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  vector<int> sortByBits(vector<int> &arr) {
+    sort(arr.begin(), arr.end(), [&](int &a, int &b) {
+      int pa = __builtin_popcount(a), pb = __builtin_popcount(b);
+      if (pa == pb)
+        return a < b;
+      return pa < pb;
+    });
+    return arr;
+  }
+};
