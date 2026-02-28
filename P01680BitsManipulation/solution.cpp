13fd42d590230a4f373c456e7f8fdcb4813a6599
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2026-02-28 15:41:38 +0200

    Crear P01680BitsManipulation/solution.cpp

---
+++ P01680BitsManipulation/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        int mod = 1e9 + 7;
+        long long ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int l = log2(i) + 1;
+            ans = (((ans << l) % mod) + i) % mod;
+        }
+        return ans;
+    }
+};
