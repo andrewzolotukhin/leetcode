71c2488fc46b29e7dbdf0644eb9c04a80050cc17
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2026-03-24 17:15:04 +0200

    Crear P02906Prefix/solution.cpp

---
+++ P02906Prefix/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> prefix(n * m + 1, 1), postfix(n * m + 1, 1);
+
+        for (int i = 0, k = 1; i < n; i++) {
+            for (int j = 0; j < m; j++, k++) {
+                prefix[k] = (prefix[k - 1] * (grid[i][j] % 12345)) % 12345;
+            }
+        }
+
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+
+        for (int i = n * m - 1; i >= 0; i--) {
+            int r = i / m, c = i % m;
+            postfix[i] = (postfix[i + 1] * (grid[r][c] % 12345)) % 12345;
+        }
+
+        for (int i = 0, l = 1; i < n; i++) {
+            for (int j = 0; j < m; j++, l++) {
+                ans[i][j] = ((prefix[l - 1] * postfix[l])) % 12345;
+            }
+        }
+        return ans;
+    }
+};
