c50376d08d4a4439fb9668852267c683d491ffe0
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2026-04-05 21:06:59 +0300

    Crear P00657/solution.cpp

---
+++ P00657/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0, n = moves.size();
+        for (int i = 0; i < n; i++) {
+            char c = moves[i];
+            if (c == 'L')
+                x--;
+            if (c == 'R')
+                x++;
+            if (c == 'U')
+                y--;
+            if (c == 'D')
+                y++;
+        }
+        return x == 0 && y == 0;
+    }
+};
